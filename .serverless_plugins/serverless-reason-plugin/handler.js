a23498725456390fa09616d8ddab65abdb60a8ff
Nik Graf <nik@nikgraf.com>
2017-02-12 00:23:13 +0100

    make sure it compiles reason on deploy

--- .serverless_plugins/serverless-reason-plugin/handler.js
+++ .serverless_plugins/serverless-reason-plugin/handler.js
@@ -2,13 +2,8 @@
 
 const fs = require('fs');
 const child = require('child_process');
-// TODO remove
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
 
 module.exports.run = (event, context, callback) => {
-
-  // console.log(fs.readdirSync(__dirname))
-
   var proc = child.spawn('./Index.native')
   proc.on('exit', function(code, signal) {
     callback(null, { message: 'yay' })
--- .serverless_plugins/serverless-reason-plugin/index.js
+++ .serverless_plugins/serverless-reason-plugin/index.js
@@ -12,7 +12,7 @@ class ServerlessPlugin {
 
     this.commands = {
       build: {
-        usage: 'Build the reason targets',
+        usage: 'Build the reason targets (also done on deploy)',
         lifecycleEvents: [
           'build',
         ],
@@ -21,39 +21,59 @@ class ServerlessPlugin {
 
     this.hooks = {
       'build:build': this.build.bind(this),
+      'after:deploy:initialize': this.build.bind(this),
     };
   }
 
   build() {
-    this.serverless.cli.log('Build Test.native');
-
     const nodeModulesBinPath = path.join(process.cwd(), 'node_modules', '.bin')
     const envPath = `${process.env.PATH}:${nodeModulesBinPath}`
     const env = Object.assign({}, process.env, { PATH: envPath })
 
-    // replace with Rambda
+    const promises = []
     Object.keys(this.serverless.service.functions).map((key) => {
       const func = this.serverless.service.functions[key]
       if (func.compileReason === undefined || func.compileReason !== 'native') {
         return;
       }
 
+      // Compile the Reason file for this specific function
+      this.serverless.cli.log(`Compile ${func.handler}.re for the function '${key}'...`);
       const filePath = `./${func.handler}.native`
       execSync(`eval $(dependencyEnv) && nopam && rebuild -use-ocamlfind -cflag -w -cflag -40 -I . ${filePath} 2>&1 | berror.native --path-to-refmttype refmttype`, { stdio:[0,1,2], env });
 
+      const serverlessDir = path.join(process.cwd(), '.serverless')
       func.handler = 'handler.run'
-      func.artifact = path.join(process.cwd(), '.serverless', `${key}.zip`)
+      func.package = {
+        artifact: path.join(serverlessDir, `${key}.zip`)
+      }
 
-      const output = fs.createWriteStream(func.artifact)
+      // Create the .serverless directory if necessary
+      if (!fs.existsSync(serverlessDir)){
+        fs.mkdirSync(serverlessDir)
+      }
+
+      // Package the function
+      const output = fs.createWriteStream(func.package.artifact)
       const archive = archiver('zip', {
         store: true // Sets the compression method to STORE.
       })
       archive.pipe(output)
-      archive.append(fs.createReadStream(filePath), { name: 'Index.native' })
+      // Add the Binary to the zip and make it executable
+      archive.append(fs.createReadStream(filePath), { name: 'Index.native', mode: 777 })
+      // Add the JS shim to the zip
       const handlerPath = path.join(__dirname, 'handler.js')
       archive.append(fs.createReadStream(handlerPath), { name: 'handler.js' })
       archive.finalize()
+
+      const promise = new Promise((zipResolve, zipReject) => {
+        output.on('close', () => zipResolve());
+        archive.on('error', (err) => zipReject(err));
+      })
+      promises.push(promise)
     })
+
+    return Promise.all(promises)
   }
 }
 
